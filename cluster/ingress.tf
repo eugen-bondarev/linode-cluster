3fd7ddd4aaeff5bd37a4a62b0c13f4dbbb581892
Eugen Bondarev <eug.bondarev@gmail.com>
2023-07-24 20:21:05 +0200

    Making terraform forget DNS zone resource. Otherwise, I sometimes end up destroying it and having to wait up to 48 hours for the new DNS configs to propagate

--- cluster/ingress.tf
+++ cluster/ingress.tf
@@ -10,23 +10,16 @@ resource "helm_release" "nginx" {
   }
 }
 
-resource "google_dns_managed_zone" "default_zone" {
-  depends_on = [data.kubernetes_service_v1.ingress_nginx_controller]
-  name       = "default-zone"
-  dns_name   = "eugen-bondarev.com."
-}
-
 resource "google_dns_record_set" "common_dns" {
   for_each = toset([
     "eugen-bondarev.com",
     "metrics.eugen-bondarev.com",
     "pipelines.eugen-bondarev.com",
   ])
-  depends_on   = [google_dns_managed_zone.default_zone]
   name         = "${each.value}."
   type         = "A"
   ttl          = 300
-  managed_zone = google_dns_managed_zone.default_zone.name
+  managed_zone = "default-zone"
   rrdatas      = [data.kubernetes_service_v1.ingress_nginx_controller.status.0.load_balancer.0.ingress.0.ip]
 }
 
