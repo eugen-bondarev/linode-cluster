df46911bcc551f0f01f7a6e253bc777ba1944bd2
Eugen Bondarev <eug.bondarev@gmail.com>
2023-07-30 18:48:09 +0200

    Removing portfolio

--- cluster/main.tf
+++ cluster/main.tf
@@ -38,43 +38,43 @@ resource "kubernetes_namespace_v1" "namespaces" {
   }
 }
 
-resource "helm_release" "portfolio" {
-  depends_on    = [data.kubernetes_service_v1.ingress_nginx_controller]
-  name          = "portfolio"
-  chart         = "./apps/portfolio"
-  namespace     = "portfolio"
-  recreate_pods = true
+# resource "helm_release" "portfolio" {
+#   depends_on    = [data.kubernetes_service_v1.ingress_nginx_controller]
+#   name          = "portfolio"
+#   chart         = "./apps/portfolio"
+#   namespace     = "portfolio"
+#   recreate_pods = true
 
-  set {
-    name  = "github_key"
-    value = base64encode(file("${abspath(path.root)}/secrets/github-key"))
-  }
+#   set {
+#     name  = "github_key"
+#     value = base64encode(file("${abspath(path.root)}/secrets/github-key"))
+#   }
 
-  set {
-    name  = "github_key_pub"
-    value = file("${abspath(path.root)}/secrets/github-key.pub")
-  }
+#   set {
+#     name  = "github_key_pub"
+#     value = file("${abspath(path.root)}/secrets/github-key.pub")
+#   }
 
-  set {
-    name  = "host"
-    value = "eugen-bondarev.com"
-  }
+#   set {
+#     name  = "host"
+#     value = "eugen-bondarev.com"
+#   }
 
-  set {
-    name  = "db.name"
-    value = var.db.name
-  }
+#   set {
+#     name  = "db.name"
+#     value = var.db.name
+#   }
 
-  set {
-    name  = "db.host"
-    value = var.db.host
-  }
+#   set {
+#     name  = "db.host"
+#     value = var.db.host
+#   }
 
-  set {
-    name  = "db.root_password"
-    value = var.db.root_password
-  }
-}
+#   set {
+#     name  = "db.root_password"
+#     value = var.db.root_password
+#   }
+# }
 
 module "infrastructure-overview" {
   depends_on = [kubernetes_namespace_v1.namespaces]
