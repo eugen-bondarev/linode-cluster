a0a1921453075371e147040e082fd0dc1d896c03
Eugen Bondarev <eug.bondarev@gmail.com>
2023-08-05 18:48:06 +0200

    Removing unused stuff

--- cloud/main.tf
+++ cloud/main.tf
@@ -31,33 +31,7 @@ resource "linode_lke_cluster" "cluster1" {
   }
 }
 
-// Export this cluster's attributes
 output "kubeconfig" {
   value     = linode_lke_cluster.cluster1.kubeconfig
   sensitive = true
-}
-
-resource "local_file" "kubeconfig" {
-  content  = base64decode(linode_lke_cluster.cluster1.kubeconfig)
-  filename = "./../secrets/kubeconfig"
-}
-
-output "api_endpoints" {
-  value = linode_lke_cluster.cluster1.api_endpoints
-}
-
-# output "nodebalancer_ip" {
-#   value = linode_nodebalancer.cluster1_main_nodebalancer.ipv4
-# }
-
-output "status" {
-  value = linode_lke_cluster.cluster1.status
-}
-
-output "id" {
-  value = linode_lke_cluster.cluster1.id
-}
-
-output "pool" {
-  value = linode_lke_cluster.cluster1.pool
 }
